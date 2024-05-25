eefc731464ac58ba19dee199e4a421fb846b2b7b
Eugene Tang <etangnrc@gmail.com>
2024-05-25 17:16:09 +1000

    Test FB login.

--- script.js
+++ script.js
@@ -21,12 +21,12 @@ document.addEventListener('DOMContentLoaded', function() {
 function submitForm(event) {
   event.preventDefault();
 
+  console.log("Logging in...");
   FB.login(function(response) {
     if (response.authResponse) {
       let accessToken = response.authResponse.accessToken;
-      let query = document.getElementById('query').value;
-
-      console.log("Login successful");
+      console.log('Access Token:', accessToken);
+      alert('Logged in successfully!');
     } else {
       alert('User cancelled login or did not fully authorise.');
     }
