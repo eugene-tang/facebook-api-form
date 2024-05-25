7ba4024865faaf1d874fea0adf1cde22821d6429
Eugene Tang <etangnrc@gmail.com>
2024-05-25 18:33:10 +1000

    Move FB SDK into html.

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '408342358854411',
-      cookie     : true,
-      xfbml      : true,
-      version    : 'v20.0'
-    });
-    console.log('Facebook SDK initialized');
-  };
-
-  (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
-});
-
 function submitForm(event) {
   event.preventDefault();
 
